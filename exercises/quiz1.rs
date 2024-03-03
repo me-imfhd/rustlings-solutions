71e95bc8239b0369dbe5deec93b634814b31332d
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-03-03 11:31:32 +0530

    modules done

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: u32) -> u32 {
+    match num {
+        n if n > 40 => num,
+        _ => num *2
+    }
+}
 
 // Don't modify this function!
 #[test]
